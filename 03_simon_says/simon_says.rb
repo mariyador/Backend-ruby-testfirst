6ab4a1cc619c6f8efd45273049dc4330caf5ad99
mariyador <aniknorod@gmail.com>
2024-07-15 22:30:47 -0400

    03_simons_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
 #write your code here
+def echo(input)
+    "#{input}"
+end
+
+def shout(input)
+   input.upcase
+end
+
+def repeat(input, number = 2)
+    ([input] * number).join(' ')
+end
+
+def start_of_word(input, number = 1)
+    input.slice(0, number)
+end
+
+def first_word(input)
+    input.split.first
+end
+
+def titleize(input)
+    little_words = %w[a an and the or for of over]
+    words = input.split
+    words.first.capitalize!
+    words.map { |word| little_words.include?(word) ? word : word.capitalize }.join(' ')
+end
