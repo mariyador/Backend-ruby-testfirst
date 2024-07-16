a80db81469c0bcedf7c2efc06f622563a126873b
mariyador <aniknorod@gmail.com>
2024-07-15 22:02:28 -0400

    fix 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(fahrenheit)
+    celsius = (fahrenheit - 32) * 5.0 / 9.0 
+    celsius.round 
+end
+
+def ctof(celsius)
+    fahrenheit = celsius * 9.0 / 5.0 + 32.0
+    fahrenheit.round(1)
+end
