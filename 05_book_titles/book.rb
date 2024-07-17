98a5c1b6a53489654a5cb00e71d7a6d443aad43f
mariyador <aniknorod@gmail.com>
2024-07-16 22:52:25 -0400

    fix 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,22 @@
 class Book
-# write your code here
-end
+    # write your code here
+        attr_accessor :title
+    
+        def title=(word)
+            @title = capitalize(word)
+        end
+    
+        private
+        def capitalize(title)
+            little_words = ['and', 'over', 'the', 'a', 'an', 'of', 'in']
+            arr = title.split
+            arr.map!.with_index do |word, index|
+                if index == 0 || !little_words.include?(word)
+                    word.capitalize
+                else
+                    word
+                end
+            end
+        arr.join(' ')
+    end
+ end
